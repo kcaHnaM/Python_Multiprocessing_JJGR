ad7ff4701c588b4967d1bd8f05a3252e0b225c92
TheTrenix_Linux <kcahnam24@gmail.com>
2022-05-25 18:39:17 -0500

    commit "Cambios_Linux"

--- 1-sumaEREW.py
+++ 1-sumaEREW.py
@@ -1,15 +1,15 @@
 '''Programa 1: SUMA EREW
 Estudiante: José Juan García Romero'''
 
-from multiprocessing import Process
+import multiprocessing
 import math
 import time
 import os
 
-def executeThread(i,j,A):
+def executeMultProc(i,j,A):
     if (((2*j)%(math.pow(2,i))) == 0):
         A[2*j] = A[2*j] + A[((2*j)-((int) (math.pow(2,i-1))))]
-    time.sleep(1)
+    
 
 def print_titulo():
     for i in range (1,41):
@@ -23,7 +23,6 @@ def cls_screen():
         os.system("clear")
 
 def main():
-
     A = [0,5,2,10,1,8,12,7,3]
 
     cls_screen()
@@ -31,19 +30,15 @@ def main():
     print('\tPrograma 1. SUMA EREW\n')
     print_titulo()
 
-    print(A)
+    print(A[1:len(A)])
 
     a = len(A)-1
-    threads = []
     log = int(math.log(a,2))
     for i in range(1,log+1):
         for j in range(1,(int)(a/2)+1):
-            thread = threading.Thread(target=executeThread,args=(i,j,A))
-            threads.append(thread)
-            thread.start()
+            multproc = multiprocessing.Process(target=executeMultProc,args=(i,j,A))
+            multproc.start()
         
-        for hilo in threads:
-            hilo.join()
 
         print(A[1:a+1])
     
